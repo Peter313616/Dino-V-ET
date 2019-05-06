9d2c4c17e554058716a956e27c0cb87da1934632
Peter313616 <36234285+Peter313616@users.noreply.github.com>
2019-05-06 14:13:11 -0400

    Update Player.cs

--- Player.cs
+++ Player.cs
@@ -1,53 +1,70 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace ETstrikesBack
 {
     public class Player
-
     {
-        Canvas canvas;
-        Window window;
         public Rectangle rectangle;
-        public Point pos = new Point(100, 100);
-        public Player(Canvas c, Window w)
+        public Canvas canvas;
+        public Point pos = new Point(285, 500);
+        int pSpeed = 4;
+        public Player(Rectangle r, Canvas c)
         {
+            rectangle = r;
             canvas = c;
-            window = w;
-            rectangle = new Rectangle();
             rectangle.Height = 30;
             rectangle.Width = 30;
             rectangle.Fill = Brushes.Blue;
-            Canvas.SetLeft(rectangle, pos.X);
-            Canvas.SetTop(rectangle, pos.Y);
-            canvas.Children.Add(rectangle);
+            c.Children.Add(r);
         }
-        public void pMovement(Key key)
+
+        public void pMovement()
         {
-            if(key == Key.Up)
+            if (Keyboard.IsKeyDown(Key.Up))
             {
-                pos.Y -= 30;
+                if (pos.Y >= 0)
+                {
+                    pos.Y -= pSpeed;
+                }
             }
-            if(key == Key.Down)
+            else if (Keyboard.IsKeyDown(Key.Down))
             {
-                pos.Y += 30;
+                if (pos.Y <= 530)
+                {
+                    pos.Y += pSpeed;
+                }
             }
-            if(key == Key.Left)
+            else if (Keyboard.IsKeyDown(Key.Left))
             {
-                pos.X -= 30;
+                if (pos.X >= 5)
+                {
+                    pos.X -= pSpeed;
+                }
+
             }
-            if(key == Key.Right)
+            else if (Keyboard.IsKeyDown(Key.Right))
             {
-                pos.X += 30;
+                if (pos.X <= 550)
+                {
+                    pos.X += pSpeed;
+                }
             }
             Canvas.SetLeft(rectangle, pos.X);
             Canvas.SetTop(rectangle, pos.Y);
+            
         }
     }
 }
